a771f0485cde1d312856f6a86abedf07472f5eff
Oleg Starichenko <staro.oleg@gmail.com>
2023-05-14 15:25:03 +0300

    добавлена секция services

--- js/main.js
+++ js/main.js
@@ -28,6 +28,7 @@ function linkAction() {
 	// Когда мы нажимаем на каждый nav__link, мы удаляем класс show-menu
 	navMenu.classList.remove('show-menu')
 }
+
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
@@ -52,10 +53,47 @@ skillsHeader.forEach((el) => {
 })
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+	tabContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab => {
+	tab.addEventListener('click', () => {
+		const target = document.querySelector(tab.dataset.target)
+
+		tabContents.forEach(tabContent => {
+			tabContent.classList.remove('qualification__active')
+		})
+		target.classList.add('qualification__active')
+
+		tabs.forEach(tab => {
+			tab.classList.remove('qualification__active')
+		})
+		tab.classList.add('qualification__active')
+	})
+})
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+	modalBtns = document.querySelectorAll('.services__button'),
+	modalCloses = document.querySelectorAll('.services__modal-close')
 
+let modal = function (modalClick) {
+	modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+	modalBtn.addEventListener('click', () => {
+		modal(i)
+	})
+})
+
+modalCloses.forEach((modalClose) => {
+	modalClose.addEventListener('click', () => {
+		modalViews.forEach((modalView) => {
+			modalView.classList.remove('active-modal')
+		})
+	})
+})
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
